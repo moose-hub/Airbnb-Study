5d1899e5e48abdcddeb63b1ac478a44658ed6506
moose-hub <infamollagames@gmail.com>
2024-01-24 17:48:54 +0000

    Created fixed nav footer as well as map button

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
             Date: "22-27Apr",
             Price: 277,
             Rating: 4.89,
+            Url: "https://www.airbnb.co.uk/rooms/20958243?adults=2&children=0&enable_m3_private_room=true&infants=0&pets=0&check_in=2024-04-14&check_out=2024-04-19&source_impression_id=p3_1706059106_T1%2FRViSI3%2B6acTRV&previous_page_section_name=1000&federated_search_id=f707ebfb-ae25-4f20-9970-c57f322a9fc2",
             Images: [
                 "assets/Holiday 1/1.jpg",
                 "assets/Holiday 1/2.jpg",
@@ -19,6 +20,7 @@ document.addEventListener("DOMContentLoaded", function() {
             Date: "28Jan - 2Feb",
             Price: 236,
             Rating: 4.96,
+            Url: "https://www.airbnb.co.uk/rooms/38874907?adults=2&category_tag=Tag%3A8188&children=0&enable_m3_private_room=true&infants=0&pets=0&photo_id=931140223&check_in=2024-02-18&check_out=2024-02-23&source_impression_id=p3_1706059486_mAMFtNiHpMw6011F&previous_page_section_name=1000&federated_search_id=ab3737b0-bd98-4f2f-9dbe-a2972595de7a",
             Images: [
                 "assets/Holiday 2/1.jpg",
                 "assets/Holiday 2/2.jpg",
@@ -33,6 +35,7 @@ document.addEventListener("DOMContentLoaded", function() {
             Date: "18-23Feb",
             Price: 578,
             Rating: 4.26,
+            Url: "https://www.airbnb.co.uk/rooms/625995554253317728?adults=1&category_tag=Tag%3A8188&children=0&enable_m3_private_room=true&infants=0&pets=0&photo_id=1399919845&search_mode=flex_destinations_search&check_in=2024-02-18&check_out=2024-02-23&source_impression_id=p3_1706059582_oQ8kBj%2BGRg570AWe&previous_page_section_name=1000&federated_search_id=c7b81ec4-de7e-4747-95a1-0f0bf2346675",
             Images: [
                 "assets/Holiday 3/1.jpg",
                 "assets/Holiday 3/2.jpg",
@@ -47,6 +50,7 @@ document.addEventListener("DOMContentLoaded", function() {
             Date: "25Feb-1Mar",
             Price: 432,
             Rating: 4.92,
+            Url: "https://www.airbnb.co.uk/rooms/676044415326884478?adults=1&children=0&enable_m3_private_room=true&infants=0&pets=0&check_in=2024-02-16&check_out=2024-02-21&source_impression_id=p3_1706059606_V2rLqq2lWDJPKeHW&previous_page_section_name=1000&federated_search_id=ffbaaab1-b672-49c4-868d-5673479bf023",
             Images: [
                 "assets/Holiday 4/1.jpg",
                 "assets/Holiday 4/2.jpg",
@@ -60,6 +64,7 @@ document.addEventListener("DOMContentLoaded", function() {
             Date: "25-30Jan",
             Price: 558,
             Rating: 4.95,
+            Url: "https://www.airbnb.co.uk/rooms/2922931?adults=1&children=0&enable_m3_private_room=true&infants=0&pets=0&check_in=2024-02-01&check_out=2024-02-06&source_impression_id=p3_1706059631_yecnhG0mIFu14D2B&previous_page_section_name=1000&federated_search_id=f218c8af-3499-45e7-948d-cdfc842bcbd9",
             Images: [
                 "assets/Holiday 5/1.jpg",
                 "assets/Holiday 5/2.jpg",
@@ -70,9 +75,13 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     ]
 
+    // currency selector (unsure if using)
     let currencies = ["£", "$", "€", "¥"];
+
+    // Gap between carousel images
     let imageGap = 1.5;
 
+    // Start loop through data to populate content grid
     for (let i = 0; i < holidayData.length; i++) {
         
     // Create the card element
@@ -168,7 +177,12 @@ document.addEventListener("DOMContentLoaded", function() {
     cardCarouselNextIcon.classList.add("fa-chevron-right");
     cardCarouselPrevIcon.classList.add("fa-chevron-left");
 
+    // Create link to wrap card element in
+    // var cardElementHref = document.createElement("a");
+    // cardElementHref.href = holidayData[i].Url
+
     // Append card image and information to the card element
+    // cardElementHref.appendChild(cardElement);
     cardElement.appendChild(cardCarouselElement);
     cardElement.appendChild(cardInfoElement);
     cardCarouselElement.appendChild(cardImgElement);
@@ -213,5 +227,20 @@ document.addEventListener("DOMContentLoaded", function() {
         // Initially show the first image
         showImage(currentIndex);
     }
+
+    var prevScrollY = 0;
+
+    window.addEventListener("scroll", () => {
+        var scrollY = window.scrollY;
+        let stickyFooter = document.querySelector(".sticky__footer-container");
+        
+        if (scrollY < prevScrollY) {
+            stickyFooter.style.transform = "translate(0, 0px)"
+        } else if (scrollY > prevScrollY) {
+            stickyFooter.style.transform = "translate(0, 60px)"
+        }
+
+        prevScrollY = scrollY <= 0 ? 0 : scrollY;
+    })
 
 });
